dd4a11362a57b10ad89f3a15fb662c5fcb9b18d4
zetaxell <ig22wm@brocku.ca>
2024-01-26 15:48:29 -0500

    moving cubes

--- Week 3/script.js
+++ Week 3/script.js
@@ -31,7 +31,26 @@ const sphereGeometry = new THREE.SphereGeometry(1)
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
-scene.add(testSphere)
+//testCube
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+const cubeMaterial = new THREE.MeshNormalMaterial()
+const testCube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+//cube2
+const cube2 = new THREE.Mesh(cubeGeometry, cubeMaterial)
+
+//testTorus
+const torusGeometry = new THREE.TorusGeometry(1, 0.1, 16, 100)
+const torusMaterial = new THREE.MeshNormalMaterial()
+const testTorus = new THREE.Mesh(torusGeometry, torusMaterial)
+
+scene.add(testCube)
+
+scene.add(cube2)
+
+//scene.add(testTorus)
+
+//scene.add(testSphere)
 
 /* animation loop */
 const clock = new THREE.Clock()
@@ -42,7 +61,34 @@ const animation = () => {
     const elapsedTime = clock.getElapsedTime()
 
     //animate testSphere
-    testSphere.position.z = Math.sin(elapsedTime)
+    //testSphere.position.x = Math.sin(elapsedTime)
+
+    //animate testCube
+    testCube.rotation.x = elapsedTime
+    testCube.rotation.y = elapsedTime
+    testCube.rotation.z = elapsedTime
+
+    testCube.scale.x = Math.sin(elapsedTime / 2)
+    testCube.scale.y = Math.sin(elapsedTime / 2)
+    testCube.scale.z = Math.sin(elapsedTime / 2)
+
+    testCube.position.x = Math.sin(elapsedTime) * 2
+    testCube.position.z = Math.cos(elapsedTime) * 2
+
+    //animate cube2
+    cube2.rotation.x = elapsedTime
+    cube2.rotation.y = elapsedTime
+    cube2.rotation.z = elapsedTime
+
+    cube2.scale.x = Math.sin(elapsedTime / 2)
+    cube2.scale.y = Math.sin(elapsedTime / 2)
+    cube2.scale.z = Math.sin(elapsedTime / 2)
+
+    cube2.position.x = Math.cos(elapsedTime) * 2
+    cube2.position.z = Math.sin(elapsedTime) * 2
+
+    //animate testTorus
+    //testTorus.rotation.y = elapsedTime
 
     //renderer
     renderer.render(scene, camera)
