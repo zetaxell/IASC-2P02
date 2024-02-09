9212b305401e0284690923ce7f616fec6674b31b
zetaxell <ig22wm@brocku.ca>
2024-02-09 11:06:17 -0500

    added plato prototype

---
+++ Week 5/script.js
@@ -0,0 +1,170 @@
+import * as THREE from "three"
+import * as dat from "lil-gui"
+import { OrbitControls } from "OrbitControls"
+
+/* SETUP */
+//sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+    aspectRatio: window.innerWidth / window.innerHeight
+}
+
+/* SCENE */
+//canvas
+const canvas = document.querySelector('.webgl')
+
+//scene
+const scene = new THREE.Scene()
+
+//camera
+const camera = new THREE.PerspectiveCamera(
+    75,
+    sizes.aspectRatio,
+    0.1,
+    100
+)
+camera.position.set(9.3, 3.8, 11.6)
+scene.add(camera)
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/* MESHES */
+//testPlane
+const caveMaterial = new THREE.MeshStandardMaterial({
+    color: new THREE.Color('white'),
+    side: THREE.DoubleSide
+})
+
+//cave wall
+const caveWallGeometry = new THREE.PlaneGeometry(10, 5)
+const caveWall = new THREE.Mesh(caveWallGeometry, caveMaterial)
+caveWall.rotation.y = Math.PI * 0.5
+caveWall.position.set(-5, 0, 0)
+caveWall.receiveShadow = true
+scene.add(caveWall)
+
+//barrier wall
+const barrierWallGeometry = new THREE.PlaneGeometry(10, 2)
+const barrierWall = new THREE.Mesh(barrierWallGeometry, caveMaterial)
+barrierWall.rotation.y = Math.PI * 0.5
+barrierWall.position.set(5, -1.5, 0)
+scene.add(barrierWall)
+
+//cave floor
+const caveFloorGeometry = new THREE.PlaneGeometry(10, 10)
+const caveFLoor = new THREE.Mesh(caveFloorGeometry, caveMaterial)
+caveFLoor.rotation.x = Math.PI * 0.5
+caveFLoor.position.set(0, -2.5, 0)
+scene.add(caveFLoor)
+
+//objects
+
+//torusKnot
+const torusKnotGeometry = new THREE.TorusKnotGeometry(1, 0.2)
+const torusKnotMaterial = new THREE.MeshNormalMaterial()
+const torusKnot = new THREE.Mesh(torusKnotGeometry, torusKnotMaterial)
+torusKnot.position.set(6, 1.5, 0)
+torusKnot.castShadow = true
+scene.add(torusKnot)
+
+/* LIGHTS */
+
+/*
+//ambient light
+const ambientLight = new THREE.AmbientLight(
+    new THREE.Color('white')
+)
+scene.add(ambientLight)
+*/
+
+//directional light
+const directionalLight = new THREE.DirectionalLight(
+    new THREE.Color('white'),
+    0.5
+)
+directionalLight.target = caveWall
+directionalLight.position.set(8.6, 1.7, 0)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+scene.add(directionalLight)
+
+//directional light helper
+//const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
+//scene.add(directionalLightHelper)
+
+/* UI */
+const ui = new dat.GUI()
+
+const uiObject = {}
+
+uiObject.reset = () => {
+    directionalLight.position.set(8.6, 1.7, 0)
+}
+
+//directional light
+const lightPositionFolder = ui.addFolder('directional light position')
+
+lightPositionFolder
+    .add(directionalLight.position, 'x')
+    .min(-10)
+    .max(20)
+    .step(0.1)
+
+lightPositionFolder
+    .add(directionalLight.position, 'y')
+    .min(-10)
+    .max(10)
+    .step(0.1)
+
+lightPositionFolder
+    .add(directionalLight.position, 'z')
+    .min(-10)
+    .max(10)
+    .step(0.1)
+
+lightPositionFolder
+    .add(uiObject, 'reset')
+    .name('Reset Position')
+
+/* ANIMATION LOOP */
+const clock = new THREE.Clock()
+
+//animate
+const animation = () => {
+
+    //return elapsedTime
+    const elapsedTime = clock.getElapsedTime
+
+    //animate objects
+    //torusKnot.rotation.y = elapsedTime
+    //torusKnot.position.z = Math.sin(elapsedTime * 0.5) * 2
+
+    //update directional light helper
+    //directionalLightHelper.update()
+
+    console.log(camera.position)
+
+    //controls
+    controls.update()
+
+    //renderer
+    renderer.render(scene, camera)
+
+    //request next frame
+    window.requestAnimationFrame(animation)
+}
+
+animation()
